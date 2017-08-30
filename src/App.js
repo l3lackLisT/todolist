2aba40e0e61f6759ebaf7dd80708fb4f3cffb9ac
l3lackLisT <neehay@hotmail.com>
2017-08-30 19:31:39 +0700

    add all file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './containers/Header';
+import Content from './containers/content';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+      todos: []
+    }
+  }
+  onInputText = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  addTodo = () => {
+    console.log(1)
+    const todo = {
+      text: this.state.text
+    }
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
         <Header />
+        <Content 
+          onInputText={this.onInputText}
+          text={this.state.text}
+          addTodo={this.addTodo}
+        />
+         {
+           this.state.todos.map(todo => (
+             <div>{todo.text}</div>
+           ))
+         }
       </div>
     );
   }
